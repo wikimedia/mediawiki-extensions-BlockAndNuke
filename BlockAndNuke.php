b6cdffd11733ed1f7bf52cb4b65592f08ebb4872
Kghbln <karsten@hoffmeyer.info>
2016-04-15 11:05:56 +0200

    Provide some minor tweaks
    * Extend and rephrase general file documentation
    * Consistently switch to use __DIR__
    * Extend author's array to appreciate others
    * Provide license label to show on "Special:Version"
    * Go https for link
    
    Bug: T123943
    Change-Id: Ie2a42b54b9894e8cab3f13506eb3858ded7d68ab

--- BlockAndNuke.php
+++ BlockAndNuke.php
@@ -1,42 +1,46 @@
 <?php
+/**
+ * BlockAndNuke extension by Eliora Stahl
+ */
 
-//user will get this message if the haven't installed the extension
-
+// Entry point protection
 if( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-//path of the extension
-$dir = __DIR__ . '/';
-
-//load internationalization file.
+// Load internationalization files
 $wgMessagesDirs['BlockAndNuke'] = __DIR__ . '/i18n';
-$wgExtensionMessagesFiles['BlockAndNuke'] = $dir . 'BlockandNuke.i18n.php';
+$wgExtensionMessagesFiles['BlockAndNuke'] = __DIR__ . '/BlockandNuke.i18n.php';
 
-//setup instructions
+// Register extension
 $wgExtensionCredits['specialpage'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'BlockAndNuke',
-	'description'    => 'Gives sysops the ability to mass delete users and their pages',
+	'path' => __FILE__,
+	'name' => 'BlockAndNuke',
 	'descriptionmsg' => 'blockandnuke-desc',
-	'author'         => 'Eliora Stahl',
-	'url'            => 'http://www.mediawiki.org/wiki/Extension:BlockAndNuke',
+	'author' => array(
+		'Eliora Stahl',
+		'...'
+		),
+	'url' => 'https://www.mediawiki.org/wiki/Extension:BlockAndNuke',
+	'license-name' => 'GPL-3.0+'
 );
 
-//Permissions - not recognised as admin
+// Setup permissions - not recognised as admin
 $wgGroupPermissions['sysop']['blockandnuke'] = true;
 $wgAvailableRights[] = 'blockandnuke';
 
-$wgAutoloadClasses['SpecialBlock_Nuke'] = $dir . 'BlockandNuke.body.php';
-$wgAutoloadClasses['BanPests'] = $dir . 'BanPests.php';
+// Load classes
+$wgAutoloadClasses['SpecialBlock_Nuke'] = __DIR__ . '/BlockandNuke.body.php';
+$wgAutoloadClasses['BanPests'] = __DIR__ . '/BanPests.php';
 
-//Tell MediaWiki about the new special page and its class name 'Block_Nuke'
+// Setup special page and its class name 'Block_Nuke'
 $wgSpecialPages['BlockandNuke'] = 'SpecialBlock_Nuke';
 
+// Extension parameters
 $wgBaNwhitelist = __DIR__ . "/whitelist.txt";
-
 $wgBaNSpamUser = "Spammer";
 
+// Register hooks
 $wgHooks['PerformRetroactiveAutoblock'][] = function ($block, $blockIds) {
 	return true;
 };
