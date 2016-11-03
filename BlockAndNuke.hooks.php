9798e28b51cf012ad79929ba264ae70641bea8bb
MtDu <justin.d128@gmail.com>
2016-11-03 17:03:04 -0500

    Revert BlockAndNuke to manifest_version_1
    Bug: T149759
    Change-Id: I7e2d6506e0972eabd4b93733b8d65a8824584aa7
    (cherry picked from commit 88a7da38a81fce536ac5a21d16a2b910364c9a91)

--- BlockAndNuke.hooks.php
+++ BlockAndNuke.hooks.php
@@ -8,4 +8,11 @@ class BlockAndNukeHooks {
 	public static function onLanguageGetSpecialPageAliases( &$specialPageAliases, $langCode ) {
 		$specialPageAliases['blockandnuke'] = array( 'BlockandNuke' );
 	}
+
+	public static function onRegistration() {
+		global $wgBaNwhitelist;
+		if ( $wgBaNwhitelist === null ) {
+			$wgBaNwhitelist = __DIR__ . '/whitelist.txt';
+		}
+	}
 }
