7d26781735601775698c058a626c80ab77a59e5a
Umherirrender <umherirrender_de.wp@web.de>
2019-08-19 18:57:55 +0200

    Use Maintenance::addDescription
    It is better to use a setter function than setting a property
    
    Change-Id: Iddf1d6d49b1f5d3bad298323f17100e46eb77b49

--- maintenance/blockAndNukeSpammers.php
+++ maintenance/blockAndNukeSpammers.php
@@ -18,7 +18,7 @@ class BanHammer extends Maintenance {
 		$this->requireExtension( "BlockAndNuke" );
 		$this->addOption( "hammer", "Actually ban and nuke the entries, will dry run otherwise" );
 		$this->addOption( "brief",  "Skip all the nitty-gritty details" );
-		$this->mDescription = "Block and Nuke recent users not found in the whitelist.";
+		$this->addDescription( "Block and Nuke recent users not found in the whitelist." );
 	}
 
 	public function maybeOutput( $str ) {
